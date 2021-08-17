2f4dc30439c189e923081f021fae5226ff9c08cd
matthew_r_rowe <matthew.r.rowe@oracle.com>
2021-08-17 09:25:28 +0100

    Fixed twine issues

--- setup.py
+++ setup.py
@@ -7,22 +7,26 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 # PROJECT SPECIFIC
-NAME = "MACEst"
+NAME = "macest"
 PACKAGES = find_packages(where="src/")
 REQUIRED = Path("requirements.txt").read_text().splitlines()
 TEST_REQUIRED = Path("test_requirements.txt").read_text().splitlines()
 EXTRAS = {'tests': [TEST_REQUIRED]}
 
 
-version = Path("src/macest/__version__.py").read_text().split("__version__ = ")[-1].strip('"')
+version = Path("src/macest/__version__.py").read_text().split()[-1].strip('"')
 
 setup(
-    name='MACEst',
+    name=NAME,
+    author="Mathew Rowe @ Oracle",
+    author_email="matthew.r.rowe@oracle.com",
+    url="https://github.com/oracle/macest",
     version=version,
     packages=PACKAGES,
     package_dir={"": "src"},
     license='??',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=REQUIRED,
     extras_requires=EXTRAS,
 )
